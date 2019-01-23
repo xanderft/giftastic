f9e9562d8880886b7a0391b2494fc375f97854b8
xanderft <alexander.bmw325is@gmail.com>
2019-01-22 21:17:08 -0500

    Added comments

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -27,24 +27,33 @@ $(document).ready(function() {
    // animal then display still images and their ratings
    $(".animalButtons").on("click", function(event) {
        var animalToFind = event.target.innerText;
+	   // This is how I clear the div where the animal pictures go
        $(".animalsView").empty();
 
+	   // I based my jQuery AJAX on the W3Schools examples at https://www.w3schools.com/jquery/jquery_ajax_get_post.asp
        $.get("https://api.giphy.com/v1/gifs/search?api_key=3RAdvavxhi69zT49G8Fcr2QE3nIIdz4a&q=" + animalToFind + "&limit=10&offset=0&rating=PG&lang=en", function(response) {
            //console.log(response);
+		   // Array to hold the moving gif links
            var moveUrl = [];
+		   
            for(var i = 0; i<response.data.length; i++) {
                var animalDiv = $("<div id = 'animal'>");
+			   // This variable could be changed to an array and populated like the moveUrl
                var stillUrl = response.data[i].images.fixed_height_still.url;
                var rating = response.data[i].rating;
                var img = $("<img>").attr("src", stillUrl);
                var ratingEl = $("<p>").text("Rating: " + rating);
 
+			   // This is where I set the data-value to assist in changing from still to moving gif
                img.attr("data-value",i);
                animalDiv.append(ratingEl,img);
                moveUrl.push(response.data[i].images.fixed_height.url);
 
                $(".animalsView").append(animalDiv);
            }
+		   // This function will change the still image to a moving gif.
+		   // I do this by looking at the data-value in the img attribute
+		   // and using the moveUrl array set the corresponding gif.
            $("img").click(function() {
             var index = $(this).data("value");
             //console.log(index);
